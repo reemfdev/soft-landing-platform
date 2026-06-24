8f172a1de021f002585932976ec5ab2d61610bd7
zk1o <z.k.o.alghamdi@gmail.com>
2026-06-24 18:34:40 +0300

    fix email.service.js

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,46 +1,32 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
-async function sendOTP(
-  email,
-  otp
-) {
-
+async function sendOTP(email, otp) {
   await transporter.sendMail({
-
-    from: process.env.EMAIL_USER,
-
+    from: `"Soft Landing" <${process.env.EMAIL_USER}>`,
     to: email,
-
     subject: "OTP Verification",
-
     html: `
       <div>
-
-      <h2>Verification Code</h2>
-
-      <p>Your code:</p>
-
-      <h1>${otp}</h1>
-
-      <p>
-      Valid for 5 minutes
-      </p>
-
+        <h2>Verification Code</h2>
+        <p>Your code:</p>
+        <h1>${otp}</h1>
+        <p>Valid for 5 minutes</p>
       </div>
     `
   });
-
 }
 
-module.exports = {
-  sendOTP
-};
+module.exports = { sendOTP };
