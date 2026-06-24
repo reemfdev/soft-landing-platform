9cbe4c97383a42a073b64505ad42c34b9614cbe4
Malak Alotaibi <malakalotaibi83@gmail.com>
2026-06-24 13:58:59 +0300

    fix frontend localhost api

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -291,11 +291,11 @@ return;
  // إنشاء حساب
 try {
 
-  const res = await fetch('${import.meta.env.VITE_API_URL}/auth/register-with-company', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
+ const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/register-with-company`, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  },
 
 body: JSON.stringify({
   name,
